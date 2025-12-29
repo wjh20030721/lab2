57672f91aa86a344c829de034fac7fff4551ceff
wjh20030721 <3339146175@qq.com>
2025-12-29 15:57:23 +0800

    test

--- src/com/editor/application/view/tree/ConsoleTreeView.java
+++ src/com/editor/application/view/tree/ConsoleTreeView.java
@@ -16,6 +16,10 @@ public class ConsoleTreeView {
         printNode(root, "", true);
     }
 
+    public void print(TreeNode root, String prefix, boolean showChildren) {
+
+    }
+
     // 递归打印逻辑
     private void printNode(TreeNode node, String prefix, boolean isLast) {
         // 1. 打印当前节点
